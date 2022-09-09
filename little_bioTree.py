f2ef4d2d66ecf7b459cefda9cd2a4772a9b7b25c
L5_TA_CarlosMorantesAriza <97129975+CarlosMorantesAriza@users.noreply.github.com>
2022-09-08 22:37:33 -0400

    Update little_bioTree.py

--- little_bioTree.py
+++ little_bioTree.py
@@ -17,4 +17,4 @@ newdir=random.sample(homedirs, 1)[0]
 with open(newdir+'/bestmovie.txt', 'w') as f:
     f.write('Thank you for finding me!!!. \n This is the best movie ever, take some participation points')
 
-print('Help me, I have forgotten what is the best movie ever. The answer is saved in the file bestmovie.txt.\n The file is in one of the three directories in /home/ec2-user/, help me find it, read it and I will give you a surpise')
+print('Help me, I have forgotten what is the best movie ever. The answer is saved in the file bestmovie.txt.\n The file is in one directory in /home/ec2-user/, help me find it, read it and I will give you a surpise')
