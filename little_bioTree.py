fcdd59a0376a945d7c750884a4207e07d155af09
L5_TA_CarlosMorantesAriza <97129975+CarlosMorantesAriza@users.noreply.github.com>
2022-09-09 13:15:13 -0400

    Update little_bioTree.py

--- little_bioTree.py
+++ little_bioTree.py
@@ -17,4 +17,7 @@ newdir=random.sample(homedirs, 1)[0]
 with open(newdir+'/bestmovie.txt', 'w') as f:
     f.write('Thank you for finding me!!!. \n This is the best movie ever, take some participation points')
 
-print('Help me, I have forgotten what is the best movie ever. The answer is saved in the file bestmovie.txt.\n The file is in one directory in /home/ec2-user/, help me find it, read it and I will give you a surpise')
+# print('Help me, I have forgotten what is the best movie ever. The answer is saved in the file bestmovie.txt.\n The file is in one directory in /home/ec2-user/, help me find it, read it and I will give you a surpise')
+
+
+print('There is a text file called bestmovie.txt hidden somewhere within a directory(ies) in /home/ec2-user. Your mission is to navigate your way through these directories and find bestmovie.txt. Read the file, and tell me what the best movie is!')   
